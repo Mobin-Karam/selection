c6d8d7d27c6408970e4e9653e73fc3d10dc39b66
Mobin Karam <polyt.mobin.karam@gmail.com>
2024-01-05 11:00:46 +0330

    add interaction for show section-down

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,28 @@
 'use strict'
 
-function handleClick() {
-  document.getElementById('section-down').style.display = "block"
+const sectionUp = document.querySelectorAll('#section-up');
+let sectionDown = document.querySelectorAll('#section-down');
+
+for (let sectionUpChild = 0; sectionUpChild < sectionUp.length; sectionUpChild++) {
+  sectionUp[sectionUpChild].addEventListener('click', () => {
+    sectionDown[sectionUpChild].className === "section-down" ? sectionDown[sectionUpChild].classList.add('display-block') : sectionDown[sectionUpChild].classList.remove('display-block')
+  })
 }
+
+// switch (sectionDown.className) {
+//   case "section-down":
+//     sectionUp.addEventListener('click', () => {
+//       console.log('hello')
+//       sectionDown.classList.add('display-block');
+//       console.log(sectionDown.className)
+//     })
+//     break
+//   case "section-down display-block":
+//     sectionUp.addEventListener('click', () => {
+//       console.log('by')
+//       sectionDown.classList.remove('display-block');
+//       console.log(sectionDown.className)
+//     })
+//     break
+//   default: console.log('Have Error');
+// }
